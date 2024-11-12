921ac8f4efa9cc3b1de59c136830df79b7d7099b
Khanzada bareerah <bareerahkhanzada842@gmail.com>
2024-11-12 20:08:19 +0500

    my files

---
+++ resume1.js
@@ -0,0 +1,8 @@
+
+var typed = new Typed('#multiple-text', {
+    strings: ['Fronted Web Developer', 'Content Writer','Sketch Artist'],
+    typeSpeed:100,
+    backSpeed:100,
+    backDelay:1000,
+    loop:true
+  });
